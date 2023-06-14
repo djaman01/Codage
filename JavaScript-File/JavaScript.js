eab53bfd1c0cc479ecf3664824a44e9d524caed8
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-06-14 13:38:59 +0100

    using console.log and repairing some HTML structure errors in <head>

--- JavaScript-File/JavaScript.js
+++ JavaScript-File/JavaScript.js
@@ -1,2 +1,9 @@
+//test changement text link footer par JS
 const linkjs= document.querySelector(".linkjs");
 linkjs.innerHTML = "Text changed with JavaScript";
+
+// test résultat sur la console de la page web
+console.log (2+2);
+console.log('hey'+"!")
+console.log(3+3)
+console.log("hey: you")
